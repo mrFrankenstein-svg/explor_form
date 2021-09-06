4774a97a8df86849c6db4319b26511ac37247311
mrFrankenstein-svg <vlad.1994@list.ru>
2021-09-06 19:34:50 +0300

    дописао описание к class WriteInProgGonfigFile

--- WindowsFormsApp1/WriteInProgGonfigFile.cs
+++ WindowsFormsApp1/WriteInProgGonfigFile.cs
@@ -19,44 +19,44 @@ namespace WindowsFormsApp1
 
         public async void WIPGF(string parameter, string value)
         {
-            if (!File.Exists(progConfigDir))
+            if (!File.Exists(progConfigDir))        //если файла нет
             {
-                File.Create(progConfigDir);
-            }
+                File.Create(progConfigDir);         //создадим его
+            }   
 
-            if (File.Exists(progConfigDir))
+            if (File.Exists(progConfigDir))         //если есть
             {
-                using (StreamReader sr = new StreamReader(progConfigDir))
+                using (StreamReader sr = new StreamReader(progConfigDir))       //создадим читателя
                 {
-                    string line;
-                    for (int i = 0; i >= 0; i++)
+                    string line;            //обьявим переменную, в которую будем читать линию
+                    for (int i = 0; i >= 0; i++)            
                     {
-                        line = await sr.ReadLineAsync();
-                        if (line != null)
+                        line = await sr.ReadLineAsync();        //читаем одну линию
+                        if (line != null)           //если в линии что-то есть
                         {
-                            readableFile[i] = line;
+                            readableFile[i] = line;         //записываем в масив посторочно
                         }
                         else 
                         {
                             break;
                         }
                     }
-                    sr.Close();
+                    sr.Close();         //закрываем читателя
                 }
 
-                for (int i = 0; i < readableFile.Length; )
+                for (int i = 0; i < readableFile.Length; )          //для каждой строчки(каждого элемента массива)
                 {
-                    if (lineWitchParametr == true)
+                    if (lineWitchParametr == true)          //если совпадение уже не найдёно
                     {
                         for (int j = 0; j < readableFile[i].Length; j++)      //берём символ
                         {
                             if (readableFile[i].Substring(j, parameter.Length) == parameter)        //если такое значение найдено, то 
                             {
-                                lineWitchParametr = true;
-                                numberOfLineWitchParametr = i;
+                                lineWitchParametr = true;       //ставим идентификатор 
+                                numberOfLineWitchParametr = i;      //запоминаем строчку с параметром
                                 break;
                             }
-                            if (j + parameter.Length == readableFile[i].Length)
+                            if (j + parameter.Length == readableFile[i].Length)         //если строччка подходит к концу
                             {
                                 break;
                             }
